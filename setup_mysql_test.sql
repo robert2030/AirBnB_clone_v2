a26e72fe4b7250b2ae3f9e39f8e48e8786bd62c6
ben <benzoic09@gmail.com>
2024-01-15 12:03:06 -0800

    task 4

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -7,11 +7,10 @@ IDENTIFIED BY 'hbnb_test_pwd';
 
 -- Grant all privileges, including CREATE, on the database hbnb_test_db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* 
-TO 'hbnb_test'@'localhost'
+TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost'
- IDENTIFIED BY 'hbnb_test_pwd';
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
