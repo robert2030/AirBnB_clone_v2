123b5e0a008a02aaf9ee057290c1d67912693633
ben <benzoic09@gmail.com>
2024-01-15 11:23:03 -0800

    task 4

---
+++ setup_mysql_test.sql
@@ -0,0 +1,18 @@
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+
+-- Create the user if it doesn't exist
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+
+-- Grant all privileges, including CREATE, on the database hbnb_test_db
+GRANT ALL PRIVILEGES ON hbnb_test_db.* 
+TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
+
+-- Revoke all privileges on other databases
+REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_test'@'localhost';
+
+-- Grant SELECT privilege on the performance_schema database
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+
+-- Flush privileges to apply changes
+FLUSH PRIVILEGES;
