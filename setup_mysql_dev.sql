a357d60f8abcec750cef88c24b59d6c1e10e69e3
ben <benzoic09@gmail.com>
2024-01-15 10:59:10 -0800

    task 3

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,14 +5,12 @@ CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant all privileges, including CREATE, on the database hbnb_dev_db
-GRANT ALL PRIVILEGES ON hbnb_dev_db.*
-TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
-
--- Revoke all privileges on other databases
-REVOKE ALL PRIVILEGES ON *.* FROM 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.*
+TO 'hbnb_dev'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.*
+TO 'hbnb_dev'@'localhost';
 
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
