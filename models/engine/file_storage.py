228ab69a16b6d93eaf70259670b7c68d481db526
ben <benzoic09@gmail.com>
2024-01-15 23:53:26 -0800

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,21 +8,27 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """Returns a dictionary or list of models currently in storage"""
+        if cls is not None:
+            return {key: obj for key, obj in self.__objects.items() if isinstance(obj, cls)}
+        return self.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
+
+    def delete(self, obj=None):
+        """Deletes obj from __objects if it exists"""
+        if obj is not None:
+            key = f"{obj.__class__.__name__}.{obj.id}"
+            self.__objects.pop(key, None)
 
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
+            temp = {key: obj.to_dict() for key, obj in self.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
@@ -45,6 +51,9 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    class_name = val.get('__class__')
+                    if class_name and class_name in classes:
+                        self.__objects[key] = classes[class_name](**val)
         except FileNotFoundError:
             pass
+
