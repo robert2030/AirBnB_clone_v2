d45b679323cef0a1559ceae4367197ff2f7cdb57
ben <benzoic09@gmail.com>
2024-01-17 09:34:17 -0800

    error fix no 6

--- models/user.py
+++ models/user.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.place import Place
 
-class User(BaseModel):
+class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
@@ -13,5 +12,5 @@ class User(BaseModel):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    places = relationship('Place', backref='user',
-                         cascade='all, delete-orphan')
+    places = relationship('Place', backref='user', cascade='all, delete-orphan')
+
