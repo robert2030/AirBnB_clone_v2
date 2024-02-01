10b94d9bd605dacc6b09d0a8ed2e9c147acd6664
ben <benzoic09@gmail.com>
2024-02-01 13:37:19 -0800

    create .tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,13 +7,13 @@ from fabric.contrib.files import exists
 
 def do_pack():
     """ Generates a .tgz archive from the contents of the web_static folder.
-
     Returns:
         str: Archive path if successfully generated, None otherwise.
     """
     try:
         now = datetime.now()
-        archive_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
+        archive_name = "web_static_{}.tgz".format(now.strftime(
+            "%Y%m%d%H%M%S"))
 
         if not exists("versions"):
             local("mkdir -p versions")
