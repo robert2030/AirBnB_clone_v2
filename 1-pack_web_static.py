8e3bb215a1c94b7994f4525e0062467ea0583f20
ben <benzoic09@gmail.com>
2024-02-01 14:32:41 -0800

    create .tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,6 +3,7 @@
 from fabric.api import local, hosts
 from datetime import datetime
 from fabric.contrib.files import exists
+
 hosts_list = ['ubuntu@34.207.190.235', 'ubuntu@100.24.255.166']
 
 
@@ -12,16 +13,13 @@ def do_pack():
     Returns:
         str: Archive path if successfully generated, None otherwise.
     """
-    try:
-        now = datetime.now()
-        archive_name = "web_static_{}.tgz".format(now.strftime(
-            "%Y%m%d%H%M%S"))
+    now = datetime.now()
+    archive_name = "web_static_{}.tgz".format(now.strftime(
+        "%Y%m%d%H%M%S"))
 
-        if not exists("versions"):
-            local("mkdir -p versions")
+    if not exists("versions"):
+        local("mkdir -p versions")
 
-        local("tar -cvzf versions/{} web_static".format(archive_name))
-        return "versions/{}".format(archive_name)
-    except Exception as e:
-        print("Error packing archive:", e)
-        return None
+    local("tar -cvzf versions/{} web_static".format(archive_name))
+    return "versions/{}".format(archive_name)
+
