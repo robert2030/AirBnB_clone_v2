7d127e0e97f5f0bea6ea2a10694130e31aad4889
ben <benzoic09@gmail.com>
2024-02-01 12:33:07 -0800

    create .tgz

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,6 +5,7 @@ from fabric.api import local
 from datetime import datetime
 from fabric.contrib.files import exists
 
+
 def do_pack():
     """ Generates a .tgz archive from the contents of the web_static folder.
 
