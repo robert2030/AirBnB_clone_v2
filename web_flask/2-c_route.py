8a21cc013745d573b68728526e275f40ab3981c2
ben <benzoic09@gmail.com>
2024-02-16 10:17:45 -0800

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -16,7 +16,7 @@ def hbnb():
     return 'HBNB'
 
 
-@app.route('/c/<test>', strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def c(text):
     text = text.replace("_", " ")
     return 'C {}'.format(text)
