2975421739bb71ea7035d4a6d1ae086b18af17a4
ben <benzoic09@gmail.com>
2024-02-16 09:59:24 -0800

    web flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """script that starts a Flask web application"""
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -17,9 +17,10 @@ def hbnb():
 
 
 @app.route('/c/<test>', strict_slashes=False)
-def c_text(text):
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+def c(text):
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
